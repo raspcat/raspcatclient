d016dc312455d6a55945203a4983f2ee415d6119
raspcat <info@rasp.cat>
2014-12-22 11:11:04 +0100

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,11 +1,18 @@
 #wget -o /dev/null -O - https://raw.githubusercontent.com/raspcat/raspcatclient/master/download.sh | bash
-yes | sudo apt-get update
-yes | sudo apt-get install chromium x11-xserver-utils ttf-mscorefonts-installer
 cd ~
 wget -O r.zip https://github.com/raspcat/raspcatclient/archive/master.zip
+if [[ $? -eq 0 ]]; then
+        echo "Descarregant ..."
+else
+        echo "No es pot baixar en aquest moment, comprova connexió"
+        exit 1
+fi
+mv raspcatclient raspcatclient-master
 unzip -o -x r.zip 
 mv raspcatclient-master raspcatclient
 chmod 755 raspcatclient/raspcatclient.py 
-nohup raspcatclient/raspcatclient.py
 rm r.zip
 wget -o /dev/null -O raspcatclient/rasp_black_80.png http://rasp.cat/site-css/pictures/rasp_black_80.png
+yes | sudo apt-get update
+yes | sudo apt-get install chromium x11-xserver-utils ttf-mscorefonts-installer
+#nohup raspcatclient/raspcatclient.py
